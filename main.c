1f31e3bd886379f97de358de1b2eb9b1f9f7a789
Richard Medina <medinarichard996@gmail.com>
2021-12-08 21:14:08 -0800

    Fixed compile errors moved functions around

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 
-
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -63,44 +62,40 @@ int lsh_exit(char **args)
 {
   return 0;
 }
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+  pid = fork();
+
+  if (pid == 0) {
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    perror("lsh");
+  } else {
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
+
 int lsh_execute(char **args)
 {
   int i;
-
   if (args[0] == NULL) {
     return 1;
   }
-
   for (i = 0; i < lsh_num_builtins(); i++) {
     if (strcmp(args[0], builtin_str[i]) == 0) {
       return (*builtin_func[i])(args);
     }
   }
-
   return lsh_launch(args);
-}
-
-int main(int argc, char **argv)
-{
-  lsh_loop();
-  return EXIT_SUCCESS;
-}
-
-void lsh_loop(void)
-{
-  char *line;
-  char **args;
-  int status;
-
-  do {
-    printf("> ");
-    line = lsh_read_line();
-    args = lsh_split_line(line);
-    status = lsh_execute(args);
-
-    free(line);
-    free(args);
-  } while (status);
 }
 
 #define LSH_RL_BUFSIZE 1024
@@ -136,24 +131,9 @@ char *lsh_read_line(void)
   }
 }
 
-char *lsh_read_line(void)
-{
-  char *line = NULL;
-  ssize_t bufsize = 0;
-
-  if (getline(&line, &bufsize, stdin) == -1){
-    if (feof(stdin)) {
-      exit(EXIT_SUCCESS);
-    } else  {
-      perror("readline");
-      exit(EXIT_FAILURE);
-    }
-  }
-  return line;
-}
-
 #define LSH_TOK_BUFSIZE 64
 #define LSH_TOK_DELIM " \t\r\n\a"
+
 char **lsh_split_line(char *line)
 {
   int bufsize = LSH_TOK_BUFSIZE, position = 0;
@@ -181,23 +161,27 @@ char **lsh_split_line(char *line)
   return tokens;
 }
 
-int lsh_launch(char **args)
+void lsh_loop(void)
 {
-  pid_t pid, wpid;
+  char *line;
+  char **args;
   int status;
-  pid = fork();
 
-  if (pid == 0) {
-    if (execvp(args[0], args) == -1) {
-      perror("lsh");
-    }
-    exit(EXIT_FAILURE);
-  } else if (pid < 0) {
-    perror("lsh");
-  } else {
-    do {
-      wpid = waitpid(pid, &status, WUNTRACED);
-    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-  }
-  return 1;
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
 }
+
+int main(int argc, char **argv)
+{
+  lsh_loop();
+  return EXIT_SUCCESS;
+}
+
+
