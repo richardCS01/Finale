7d44102670d3e897c80d72bf98dcce9c75976cf1
Richard Medina <medinarichard996@gmail.com>
2021-12-08 20:32:46 -0800

    Implementedlsh_execute

--- main.c
+++ main.c
@@ -56,6 +56,22 @@ int lsh_exit(char **args)
 {
   return 0;
 }
+int lsh_execute(char **args)
+{
+  int i;
+
+  if (args[0] == NULL) {
+    return 1;
+  }
+
+  for (i = 0; i < lsh_num_builtins(); i++) {
+    if (strcmp(args[0], builtin_str[i]) == 0) {
+      return (*builtin_func[i])(args);
+    }
+  }
+
+  return lsh_launch(args);
+}
 
 int main(int argc, char **argv)
 {
