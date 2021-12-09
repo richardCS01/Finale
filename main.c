ccf7bdb28c905d3587d0fa09ae59c36b4f4e03b9
Richard Medina <medinarichard996@gmail.com>
2021-12-08 18:23:27 -0800

    Implement lsh_read_line

--- main.c
+++ main.c
@@ -53,3 +53,19 @@ char *lsh_read_line(void)
     }
   }
 }
+
+char *lsh_read_line(void)
+{
+  char *line = NULL;
+  ssize_t bufsize = 0;
+
+  if (getline(&line, &bufsize, stdin) == -1){
+    if (feof(stdin)) {
+      exit(EXIT_SUCCESS);
+    } else  {
+      perror("readline");
+      exit(EXIT_FAILURE);
+    }
+  }
+  return line;
+}
