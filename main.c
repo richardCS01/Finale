1889a3d169dfe3d05c01c5bf141aa7496c1388a1
Richard Medina <medinarichard996@gmail.com>
2021-12-08 19:00:26 -0800

    Implemented lsh_launch

--- main.c
+++ main.c
@@ -98,3 +98,24 @@ char **lsh_split_line(char *line)
   tokens[position] = NULL;
   return tokens;
 }
+
+int lsh_launch(char **args)
+{
+  pid_t pid, wpid;
+  int status;
+  pid = fork();
+
+  if (pid == 0) {
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+  } else if (pid < 0) {
+    perror("lsh");
+  } else {
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+  return 1;
+}
